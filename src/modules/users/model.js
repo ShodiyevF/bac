8bff9236dbe06868e3e78b5b82499e62a9f44ab9
ShodiyevF <axadmirzaboyev@gmail.com>
2022-07-28 14:10:47 +0500

    super admin model 20 %

--- src/modules/users/model.js
+++ src/modules/users/model.js
@@ -157,6 +157,43 @@ const companysWorkersPermissionPOSTModel = async (owner_id, user_id, { action, n
     }
 }
 
+const superAdminUsersGETModel = async (user_id) => {
+    try {
+
+        const check = await uniqRow('select * from users where user_id = $1', user_id)
+
+        const checked = check.rows.find(el => el.user_login === 'suppermupper' && el.user_password === 1114)
+        console.log();
+        const query2 = `
+        select
+        *
+        from users as u
+        inner join permissions_access as ca on ca.user_id = u.user_id
+        inner join company as c on c.company_id = u.company_id
+        where c.company_owner = $1 and u.user_id != $2;
+        `
+
+        const company = await uniqRow(query2, owner_id, user_id)
+
+        if (company.rows.length) {
+            const findedAccess = company.rows.find(el => el.action_id == action && el.permissions_names_id == name)
+            if (findedAccess) {
+                await uniqRow(`delete from permissions_access where permissions_names_id = $1 and user_id = $2 and action_id = $3`, name, user_id, action)
+                return 201
+            } else {
+                await uniqRow(`insert into permissions_access (permissions_names_id, user_id, action_id) values ($1,$2,$3)`, name, user_id, action)
+            }
+
+        } else {
+            return 400
+        }
+
+
+    } catch (error) {
+        console.log(error.message, 'companysWorkersGETModel');
+    }
+}
+
 module.exports = {
     companysGETModel,
     companysPOSTModel,
